08093039b1ab30444c64abfc4399c7a9b43f9ffb
Kayla Perez <kayla.perez270@gmail.com>
2024-07-10 16:26:56 -0700

    completed static assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
     </div>
   );
+}
+
+const data = [
+  [1, 2, 3, 4],
+  [2, 3, 4, 5],
+  [5, 6, 7, 8],
+  [6, 7, 8, 1],
+];
+
+function Board() {
+  return (
+    <>
+      <h1>Memory Game</h1>
+      <p>Find all the matching pairs of numbers!</p>
+      <ul className="board">
+        {data.map((row, i1) =>
+          row.map((el, i2) => <Card key={[i1, i2]} number={el} />)
+        )}
+      </ul>
+    </>
+  );
+}
+
+function Card({ number }) {
+  return <li className="card">{number}</li>;
 }
 
 export default App;
