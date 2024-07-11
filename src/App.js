faa8ba97a4e8aa75c91aa1dac399e5765da3b278
Kayla Perez <kayla.n.perez404@gmail.com>
2024-07-11 13:20:16 -0700

    can only flip over 2 cards

--- src/App.js
+++ src/App.js
@@ -21,33 +21,71 @@ function App() {
 }
 
 function Board({ cards }) {
+  const [flipped1, setFlipped1] = useState(null);
+  const [flipped2, setFlipped2] = useState(null);
+
+  function handleFlip(flippedCard) {
+    if (!flipped1) {
+      setFlipped1(flippedCard);
+    } else if (flipped1 && flipped1.id === flippedCard.id) {
+      setFlipped1(null);
+    } else if (!flipped2) {
+      setFlipped2(flippedCard);
+    } else if (flipped2 && flipped2.id === flippedCard.id) {
+      setFlipped2(null);
+    }
+  }
+
   return (
     <ul className="board">
       {cards.map((card) => (
-        <Card card={card} key={card.id} />
+        <Card
+          card={card}
+          key={card.id}
+          onFlip={handleFlip}
+          flipped1={flipped1}
+          flipped2={flipped2}
+        />
       ))}
     </ul>
   );
 }
 
-function Card({ card }) {
-  const [flipped, setFlipped] = useState(null);
+function Card({ card, onFlip, flipped1, flipped2 }) {
+  // const [flipped, setFlipped] = useState(null);
+
+  // function handleFlipped() {
+  //   setFlipped(card.id !== flipped ? card.id : null);
+  // }
+
+  const isFlipped = flipped1?.id === card.id || flipped2?.id === card.id;
+
   return (
     <li>
       <button
-        className={`card ${flipped ? 'flipped' : ''}`}
-        onClick={() => setFlipped(card.id !== flipped ? card.id : null)}
+        className={`card ${isFlipped ? 'flipped' : ''}`}
+        onClick={() => onFlip(card)}
       >
-        {flipped && <p>{card.number}</p>}
+        {isFlipped && <p>{card.number}</p>}
       </button>
     </li>
   );
 }
 
 function Score() {
+  const [score, setScore] = useState(0);
+
+  // function handleScore() {
+  //   if (flippedCards.length === 2) {
+  //     if (flippedCards[0].number === flippedCards[1].number) {
+  //       setScore((s) => s + 1);
+  //     }
+  //   }
+  // }
+
   return (
     <div className="score">
-      <p>Score: X</p>
+      <p>Score: {score}</p>
       <p>Pairs left: X</p>
     </div>
   );
